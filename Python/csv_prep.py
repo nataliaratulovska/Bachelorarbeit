3df20d48762eb6527b33ea386315b134013c753b
Natalia <154248394+nataliaratulovska@users.noreply.github.com>
2024-04-29 19:57:44 +0200

    CSV Erweiterung

--- Python/csv_prep.py
+++ Python/csv_prep.py
@@ -19,6 +19,26 @@ df['Suspense']=df['Suspense'].apply(noslashes)
 #Hinzufügen von ID
 df.insert(0, 'id', range(len(df)))
 
+#Hinzufügen leerer Spalten für die späteren Untersuchungen, hier Fragesatz, Ellipse, kurzer/simpler Satz (Basisstruktur) und Satzlänge
+#ich glaube am einfachsten ist es wenn ich mich nur auf die Länge der Sätze festlege (Wörter pro Satz), und vergleiche ob diese in suspense Sätzen abnimmt
+df['Fragesatz']=''
+df['Satzlänge']=''
+
+#Befüllen der Fragesatz Spalte
+def isquestion(text):
+    if '?' in text:
+        return 1
+    else:
+        return 0
+df['Fragesatz']=df['Satz'].apply(isquestion)
+
+#Befüllen der Länge Spalte
+def length(text):
+    return len(text.split())
+df['Satzlänge']=df['Satz'].apply(length)
+
+
+
 #Speichern der bearbeiteten Datei
 df.to_csv('annotated csv/python_edit.csv', sep=',', index=False)
 
