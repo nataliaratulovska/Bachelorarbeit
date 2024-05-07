ed2a37b39473784a727d6bc4aab6c2ea6994349b
Natalia <154248394+nataliaratulovska@users.noreply.github.com>
2024-05-07 16:05:35 +0200

    chi2test

--- Python/Statistik.py
+++ Python/Statistik.py
@@ -1,5 +1,6 @@
 import numpy
 from scipy.stats import chi2_contingency
+from scipy.stats.contingency import association
 import pandas
 
 '''to-do:
@@ -12,11 +13,24 @@ import pandas
 #H0: Die Länge der Sätze variiert zwischen suspense und nicht-suspense Sätzen nicht.
 
 #Daten einbinden
-csv = pandas.read_csv('annotated csv/python_edit.csv')
+df = pandas.read_csv('annotated csv/python_edit.csv')
+csv = df.drop(df[df.Suspense == '-'].index)
 
 #Kontingenztabelle
+def chi2extended(Spalte1, Spalte2):
+    #Kontingenztabelle
+    con = pandas.crosstab(csv[Spalte1], csv[Spalte2])
+    #chi2test
+    chi2, p, _, _ = chi2_contingency(con)
+    # Fortführung: Kontingenzkoeffizient/Cramers V
+    pearson = association(con, method="pearson")
+    cramers = association(con, method="cramer")
+    return pandas.Series([chi2, p, pearson, cramers], index=['chi2', 'pvalue', 'pearson', 'cramers'])
 
+
+'''    
 con = pandas.crosstab(csv['Suspense'], csv['Fragesatz'])
+print(con)
 
 #chi2test
 #p=0.05; k=3,84 -> kritischer Wert aus der Chi Quadrat Verteilungstabelle bei df=1 und signifikanzniveau=0.95
@@ -26,12 +40,26 @@ chi2, p, _, _= chi2_contingency(con)
 print('chi2: ', chi2)
 print('p: ', p)
 
-#Fortführung: Kontingenzkoeffizient
+#Fortführung: Kontingenzkoeffizient/Cramers V
+print(association(con, method="pearson"))
+print(association(con, method="cramer"))
+'''
+#Für alle Erzählungen
+print(chi2extended('Suspense', 'Fragesatz'))
 
-#Berechnung n
-n= numpy.sum(con.values)
-rows, cols = con.shape
+#Ein Schritt weiter gedacht: auf einzelne Erzählungen angewendet
 
-#Berechnung Kontingenzkoeffizient
-k = numpy.sqrt(chi2/(n*(min(rows, cols)-1)))
-print('k: ',k)
+stories = csv.groupby('Titel')
+def chi2extendedgroup(group):
+    #Kontingenztabelle
+    con = pandas.crosstab(group['Suspense'].values, group['Fragesatz'].values)
+    #chi2Test
+    chi2, p, _, _ = chi2_contingency(con)
+    # Fortführung: Kontingenzkoeffizient/Cramers V
+    pearson = association(con, method="pearson")
+    cramers = association(con, method="cramer")
+    return pandas.Series([chi2, p, pearson, cramers], index=['chi2', 'pvalue', 'pearson', 'cramers'])
+
+print(stories.apply(chi2extendedgroup))
+
+
