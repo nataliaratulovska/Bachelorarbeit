f935ba980a05fe9d66a83e64a72f8153ad76d682
Natalia <154248394+nataliaratulovska@users.noreply.github.com>
2024-05-10 13:14:34 +0200

    Kontrollgruppe CSV

--- Python/csv_prep.py
+++ Python/csv_prep.py
@@ -1,43 +1,49 @@
 import pandas
 
-#Einlesen der catma csv (hier wurden bereits unbrauchbare spalten entfernt)
-#Probleme: die catma datei hatte keine Titelzeile, alles war durcheinander, musste ich händisch hinzufügen und die unbrauchbaren splaten auch entfernt)
-df=pandas.read_csv('annotated csv/catma_editedutf8.csv', sep=';')
+def csv_prep(path, newpath):
+    #Einlesen der catma csv (hier wurden bereits unbrauchbare spalten entfernt)
+    #Probleme: die catma datei hatte keine Titelzeile, alles war durcheinander, musste ich händisch hinzufügen und die unbrauchbaren splaten auch entfernt)
+    df=pandas.read_csv(path, sep=';')
 
-#sortieren nach Titel und Position des Satzes
-df=df.sort_values(by=['Titel', 'Position1'])
+    #sortieren nach Titel und Position des Satzes
+    df=df.sort_values(by=['Titel', 'Position1'])
 
-#Entfernen der überschüssigen Spalten
-rem = ['Position1', 'Position2']
-df = df.drop(rem, axis=1)
+    #Entfernen der überschüssigen Spalten
+    rem = ['Position1', 'Position2']
+    df = df.drop(rem, axis=1)
 
-#Bearbeiten der "Suspense" Spalte, hier hat Catma ein random Slah eingebaut und ich finds doof
-def noslashes(text):
-    return text.replace("/","")
-df['Suspense']=df['Suspense'].apply(noslashes)
+    #Bearbeiten der "Suspense" Spalte, hier hat Catma ein random Slash eingebaut und ich finds doof
+    def noslashes(text):
+        return text.replace("/","")
+    df['Suspense']=df['Suspense'].apply(noslashes)
 
-#Hinzufügen von ID
-df.insert(0, 'id', range(len(df)))
+    #Hinzufügen von ID
+    df.insert(0, 'id', range(len(df)))
 
-#Hinzufügen leerer Spalten für die späteren Untersuchungen, hier Fragesatz, Ellipse, kurzer/simpler Satz (Basisstruktur) und Satzlänge
-#ich glaube am einfachsten ist es wenn ich mich nur auf die Länge der Sätze festlege (Wörter pro Satz), und vergleiche ob diese in suspense Sätzen abnimmt
-df['Fragesatz']=''
-df['Satzlänge']=''
+    #Hinzufügen leerer Spalten für die späteren Untersuchungen, hier Fragesatz, Ellipse, kurzer/simpler Satz (Basisstruktur) und Satzlänge
+    #ich glaube am einfachsten ist es wenn ich mich nur auf die Länge der Sätze festlege (Wörter pro Satz), und vergleiche ob diese in suspense Sätzen abnimmt
+    df['Fragesatz']=''
+    df['Satzlänge']=''
 
-#Befüllen der Fragesatz Spalte
-def isquestion(text):
-    if '?' in text:
-        return 1
-    else:
-        return 0
-df['Fragesatz']=df['Satz'].apply(isquestion)
+    #Befüllen der Fragesatz Spalte
+    def isquestion(text):
+        if '?' in text:
+            return 1
+        else:
+            return 0
+    df['Fragesatz']=df['Satz'].apply(isquestion)
 
-#Befüllen der Länge Spalte
-def length(text):
-    return len(text.split())
-df['Satzlänge']=df['Satz'].apply(length)
+    #Befüllen der Länge Spalte
+    def length(text):
+        return len(text.split())
+    df['Satzlänge']=df['Satz'].apply(length)
 
 
-#Speichern der bearbeiteten Datei
-df.to_csv('annotated csv/python_edit.csv', sep=',', index=False)
+    #Speichern der bearbeiteten Datei
+    df.to_csv(newpath, sep=',', index=False)
 
+#Hauptgruppe
+#csv_prep('annotated csv/catma_editedutf8.csv', 'annotated csv/python_edit.csv')
+
+#Kontrollgruppe
+#csv_prep('annotated csv/catma_editedutf8_cg.csv', 'annotated csv/python_edit_cg.csv')
