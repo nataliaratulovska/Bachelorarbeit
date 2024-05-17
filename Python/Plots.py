36aaa08316fb6bd442f0aaba1870185dc0cba4eb
Natalia <154248394+nataliaratulovska@users.noreply.github.com>
2024-05-17 16:59:04 +0200

    Plot suspenseverlauf

--- Python/Plots.py
+++ Python/Plots.py
@@ -1,25 +1,65 @@
 import matplotlib.pyplot as plt
-import Statistik as s
-import numpy
-df = s.csv
-ma = max(df['Satzlänge'])
-mi = min(df['Satzlänge'])
-print(numpy.std(df['Satzlänge']))
-print(numpy.mean(df['Satzlänge']))
-#Erstellung Plot für Nachweis Normalverteilung
-plt.hist(s.csv['Satzlänge'], density=True, bins=25)
-mean = numpy.mean(df['Satzlänge'])
-std = numpy.std(df['Satzlänge'])
-x = numpy.linspace(mean - 3*std, mean + 3*std, 100)
-plt.plot(x, numpy.exp(-x**2 / 2) / numpy.sqrt(2*numpy.pi), linewidth=2, color='r')
-plt.text(0, 0.4, 'p-value = %.8f' % s.p)
+import pandas
+import numpy as np
+from scipy.interpolate import make_interp_spline
 
-plt.xlabel('Data')
-plt.ylabel('Density')
-plt.title('Normality Test')
-plt.show()
-'''Gauss-Funktion für Standardnormalverteilung:
-f(x) = (1 / (σ * √(2*π))) * e^(-1/2 * ((x - μ) / σ)^2)
-Wenn μ=0 und σ=1:
-f(x) = (1 / (√(2*π))) * e^(-1/2 * x^2)
-Diese Funktion erreicht ihr Maximum bei x=0.'''
+#Daten einbinden
+df = pandas.read_csv('annotated csv/python_edit.csv')
+df_cg = pandas.read_csv('annotated csv/python_edit_cg.csv')
+#Es werden Daten ausgelassen, die für die Analyse nicht von Bedeutung sind, z. B. die Sprechernamen im "Der Fremde", da hierdurch das Ergbnis verfälscht wird.
+#csv = df.drop(df[df.Suspense == '-'].index)
+#csv_cg = df_cg.drop(df_cg[df_cg.Suspense == '-'].index)
+def dropminus(df):
+    return df.drop(df[df.Suspense == '-'].index)
+
+#Das Bettelweib von Locarno
+#g1 = df[:33]
+g1 = dropminus(df[:33])
+#Der Fremde
+g2 = dropminus(df[34:217])
+#Der Untergang des Hauses Usher
+g3 = dropminus(df[219:])
+
+########################################################################################
+
+#Verlauf der suspense, wichtig zur Veranschaulichung der Suspense Kurven
+def suspensecurve(df,bin):
+
+    x = np.array(df['id'],dtype=float)
+    y = np.array(df['Suspense'], dtype=float)
+
+    #nutzung von bins -> sätze werden zusammengenommen und durchschintt suspense wird berechnet
+    # so wird der Verlauf der Suspense besser sichtbar, da aus kategorischer variable ('Suspense') eine numerische gemacht wird
+
+    new_x = []
+    new_y = []
+    for i in range(0, len(x), bin):
+
+        x_bin = x[i:i + bin]
+        y_bin = y[i:i + bin]
+
+        #neue Werte, berechnung mittelwert der bins
+        new_x.append(x_bin.mean())
+        new_y.append(y_bin.mean())
+
+
+    new_x = np.array(new_x)
+    new_y = np.array(new_y)
+
+    #Ausglätten der Kurve durch spline interpolation
+    x_smooth = np.linspace(new_x.min(), new_x.max(), 300)
+    spline = make_interp_spline(new_x, new_y, k=3)
+    y_smooth = spline(x_smooth)
+
+    #Plotting
+    plt.plot(x_smooth, y_smooth, linestyle='-')
+    plt.xlabel('Satz ID')
+    plt.ylabel('Dichte Suspense')
+    plt.ylim(-0.2, 1.2)
+    plt.title('Suspenseverlauf in der Erzählung '+df['Titel'].tolist()[1], pad=20)
+    plt.show()
+
+#da erzählungen alle verschieden lang sind und ich ca gleiche verzerrung haben will, ist die bin zahl rational zu anzahl instanzen
+print(suspensecurve(g1, 5))
+print(suspensecurve(g2, 28))
+print(suspensecurve(g3, 42))
