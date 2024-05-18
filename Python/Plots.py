4a31518a34c5b0b04fd54e265d2f824ee355b474
Natalia <154248394+nataliaratulovska@users.noreply.github.com>
2024-05-18 22:07:59 +0200

    Plots für Fragesätze und Länge

--- Python/Plots.py
+++ Python/Plots.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import pandas
 import numpy as np
 from scipy.interpolate import make_interp_spline
+from scipy.signal import find_peaks
 
 #Daten einbinden
 df = pandas.read_csv('annotated csv/python_edit.csv')
@@ -19,9 +20,11 @@ g1 = dropminus(df[:33])
 g2 = dropminus(df[34:217])
 #Der Untergang des Hauses Usher
 g3 = dropminus(df[219:])
+#Hauptgruppe
+gall = dropminus(df)
 
 ########################################################################################
-
+'''
 #Verlauf der suspense, wichtig zur Veranschaulichung der Suspense Kurven
 def suspensecurve(df,bin):
 
@@ -51,15 +54,114 @@ def suspensecurve(df,bin):
     spline = make_interp_spline(new_x, new_y, k=3)
     y_smooth = spline(x_smooth)
 
+    #Peaks finden
+    peaks, _ = find_peaks(y_smooth, height=0)
+    plt.plot(x_smooth[peaks], y_smooth[peaks], "x", label='Peaks')
+    for i, peak in enumerate(peaks, start=1):
+        plt.annotate(f'Peak {i}',
+                     (x_smooth[peak], y_smooth[peak]),
+                     textcoords="offset points",
+                     xytext=(0, 10),
+                     ha='center')
+
     #Plotting
     plt.plot(x_smooth, y_smooth, linestyle='-')
     plt.xlabel('Satz ID')
-    plt.ylabel('Dichte Suspense')
-    plt.ylim(-0.2, 1.2)
+    plt.ylabel('Durchschnittswert der Suspense ')
+    plt.ylim(0, 1.2)
     plt.title('Suspenseverlauf in der Erzählung '+df['Titel'].tolist()[1], pad=20)
     plt.show()
 
 #da erzählungen alle verschieden lang sind und ich ca gleiche verzerrung haben will, ist die bin zahl rational zu anzahl instanzen
-print(suspensecurve(g1, 5))
-print(suspensecurve(g2, 28))
-print(suspensecurve(g3, 42))
+print(suspensecurve(g1, 3))
+print(suspensecurve(g2, 17))
+print(suspensecurve(g3, 26))
+'''
+########################################################
+'''
+#Plots für Fragesätze
+#Zählen der Fragesätze in suspense und nicht suspense sätzen
+def ycounter(df):
+    s0_f1 = len(df[(df['Suspense'] == '0') & (df['Fragesatz'] == 1)])
+    s1_f1 = len(df[(df['Suspense'] == '1') & (df['Fragesatz'] == 1)])
+    return [s0_f1, s1_f1]
+
+s0_f1 = [ycounter(g1)[0],ycounter(g2)[0],ycounter(g3)[0],ycounter(gall)[0]]
+
+s1_f1 = [ycounter(g1)[1],ycounter(g2)[1],ycounter(g3)[1],ycounter(gall)[1]]
+
+#beschriftung und xachse
+labels = ['Das Bettelweib\nvon Locarno', 'Der Fremde', 'Der Untergang\ndes Hauses Usher', 'Gesamtgruppe']
+x = np.arange(len(labels))
+width = 0.35
+
+#diagramm
+fig, ax = plt.subplots()
+balken1 = ax.bar(x - width/2, s0_f1, width, label='kein Suspensesatz', color='mediumseagreen')
+balken2 = ax.bar(x + width/2, s1_f1, width, label='Suspensesatz', color='crimson')
+ax.set_ylabel('Anzahl der Fragesätze')
+ax.set_title('Verteilung der Fragesätze in Suspense und nicht-Suspense Sätzen', pad=20)
+ax.set_xticks(x)
+ax.set_xticklabels(labels)
+ax.legend()
+
+#beschriftung Balken
+def add_labels(balken):
+    for b in balken:
+        height = b.get_height()
+        ax.annotate('{}'.format(height),
+                    xy=(b.get_x() + b.get_width() / 2, height),
+                    xytext=(0, 1),
+                    textcoords="offset points",
+                    ha='center', va='bottom')
+add_labels(balken1)
+add_labels(balken2)
+
+plt.tight_layout()
+plt.show()
+'''
+#######################################################
+
+#Plots für Satzlänge
+#berechnen der Satzdurchschnittslänge
+def ymean(df):
+    s0=df[(df['Suspense'] == '0')]
+    s1=df[(df['Suspense'] == '1')]
+    mean_s0 = s0['Satzlänge'].mean()
+    mean_s1 = s1['Satzlänge'].mean()
+    return [round(mean_s0,2), round(mean_s1,2)]
+
+mean_s0 = [ymean(g1)[0],ymean(g2)[0],ymean(g3)[0],ymean(gall)[0]]
+
+mean_s1 = [ymean(g1)[1],ymean(g2)[1],ymean(g3)[1],ymean(gall)[1]]
+
+
+#beschriftung und xachse
+labels = ['Das Bettelweib\nvon Locarno', 'Der Fremde', 'Der Untergang\ndes Hauses Usher', 'Gesamtgruppe']
+x = np.arange(len(labels))
+width = 0.35
+
+#diagramm
+fig, ax = plt.subplots()
+balken1 = ax.bar(x - width/2, mean_s0, width, label='kein Suspensesatz', color='mediumseagreen')
+balken2 = ax.bar(x + width/2, mean_s1, width, label='Suspensesatz', color='crimson')
+ax.set_ylabel('Satzlänge in gemessen nach Wortanzahl')
+ax.set_title('Durchschnittliche Satzlänge in Suspense und nicht-Suspense Sätzen', pad=20)
+ax.set_xticks(x)
+ax.set_xticklabels(labels)
+ax.legend()
+
+#beschriftung Balken
+def add_labels(balken):
+    for b in balken:
+        height = b.get_height()
+        ax.annotate('{}'.format(height),
+                    xy=(b.get_x() + b.get_width() / 2, height),
+                    xytext=(0, 1),
+                    textcoords="offset points",
+                    ha='center', va='bottom')
+add_labels(balken1)
+add_labels(balken2)
+
+plt.tight_layout()
+plt.show()
